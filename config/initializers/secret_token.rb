533f981bc1bc11cc5c3662184d07022007d333fa
Aliaksei Kulbei <a.kulbei@gmail.com>
2013-10-26 16:35:09 +0200

    Empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oracul::Application.config.secret_key_base = '5366af4330d42ab590034f3fed9d845811a31190923b0d2e6cd3efc0729732147ce090b2a936a40e2dee9d101bee432827832ec1bf742bfbfa1de7583ef0f5c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Oracul::Application.config.session_store :cookie_store, key: '_oracul_session'
